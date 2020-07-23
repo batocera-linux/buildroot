cacf23fbf833498dc07cbf96fe52c12a43e44983
Romain Tisserand <romain.tisserand@gmail.com>
2020-07-23 11:21:59 +0200

    Bump fmt to latest 7.0.1 for yuzu (no more conflicts with citra/lr-citra)

--- package/fmt/fmt.mk
+++ package/fmt/fmt.mk
@@ -3,8 +3,7 @@
 # fmt
 #
 ################################################################################
-# Batocera - Version 6.0.0 required by citra-emu. Newer versions of fmt cause errors when starting the game
-FMT_VERSION = 6.0.0
+FMT_VERSION = 7.0.1
 FMT_SITE = $(call github,fmtlib,fmt,$(FMT_VERSION))
 FMT_LICENSE = MIT with exception
 FMT_LICENSE_FILES = LICENSE.rst
