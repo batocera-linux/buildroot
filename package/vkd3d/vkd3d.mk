898e99db48f288abc3decfcb201555168adb484d
Romain Tisserand <romain.tisserand@gmail.com>
2020-07-30 10:09:01 +0200

    Fix vkd3d building dynamic libraries

--- package/vkd3d/vkd3d.mk
+++ package/vkd3d/vkd3d.mk
@@ -12,25 +12,9 @@ VKD3D_LICENSE_FILES = COPYING.LIB LICENSE
 VKD3D_DEPENDENCIES = host-bison host-flex host-wine spirv-headers host-libtool #host-autoconf host-automake
 VKD3D_CONF_ENV += WIDL="$(BUILD_DIR)/host-wine-$(WINE_VERSION)/tools/widl/widl"
 
-VKD3D_CONF_OPTS =  --disable-shared --enable-static --disable-tests --with-sysroot=$(STAGING_DIR)
+VKD3D_CONF_OPTS = --disable-tests --with-sysroot=$(STAGING_DIR)
+
 VKD3D_AUTORECONF = YES
-
 VKD3D_INSTALL_STAGING = YES
-
-#define VKD3D_INSTALL_TARGET_CMDS
-#        $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-#                STRIP=/bin/true \
-#                DESTDIR=$(TARGET_DIR) \
-#                install
-#endef
-#
-
-#define VKD3D_INSTALL_STAGING_CMDS
-#        $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-#                PREFIX=$(TARGET_DIR)/usr \
-#                STRIP=/bin/true \
-#                DESTDIR=$(STAGING_DIR) \
-#                install
-#endef
 
 $(eval $(autotools-package))
