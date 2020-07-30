5f1ad827a7a12630f227951f9a974e76d67874ed
Romain Tisserand <romain.tisserand@gmail.com>
2020-07-30 15:13:55 +0200

    Better wine/vkd3d build process inside buildroot

--- package/spirv-headers/spirv-headers.mk
+++ package/spirv-headers/spirv-headers.mk
@@ -6,7 +6,7 @@
 
 SPIRV_HEADERS_VERSION = 1.5.3
 SPIRV_HEADERS_SOURCE = $(SPIRV_HEADERS_VERSION).tar.gz
-SPIRV_HEADERS_SITE = https://github.com/KhronosGroup/SPIRV-Headers/archive/
+SPIRV_HEADERS_SITE = https://github.com/KhronosGroup/SPIRV-Headers/archive
 
 # Only installs header files
 SPIRV_HEADERS_INSTALL_STAGING = YES
