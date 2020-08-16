4b31c5bac21860a2571faccd69e8e27ac790d428
Romain TISSERAND <romain.tisserand@gmail.com>
2020-08-16 13:02:59 +0200

    Fix buildroot slirp by moving to meson https://gitlab.freedesktop.org/slirp/libslirp/

--- package/slirp/slirp.mk
+++ package/slirp/slirp.mk
@@ -4,11 +4,16 @@
 #
 ################################################################################
 
+# batocera
+SLIRP_VERSION = v4.3.1
+SLIRP_SITE = https://gitlab.freedesktop.org/slirp/libslirp/-/archive/$(SLIRP_VERSION)
+SLIRP_SOURCE = libslirp-$(SLIRP_VERSION).tar.gz
 # There's no tarball releases of slirp, so we use the git repo
 # Also, there's no tag, so we use a random SHA1 (master's HEAD
 # of today)
-SLIRP_VERSION = 8c2da74c1385242f20799fec8c04f8378edc6550
-SLIRP_SITE = git://anongit.freedesktop.org/spice/slirp
+#SLIRP_VERSION = 8c2da74c1385242f20799fec8c04f8378edc6550
+#SLIRP_SITE = git://anongit.freedesktop.org/spice/slirp
+
 SLIRP_LICENSE = BSD-4-Clause, BSD-2-Clause
 # Note: The license file 'COPYRIGHT' is missing from the sources,
 # although some files refer to it.
@@ -16,6 +21,9 @@ SLIRP_INSTALL_STAGING = YES
 
 # As we're using the git tree, there's no ./configure,
 # so we need to autoreconf.
-SLIRP_AUTORECONF = YES
+#batocera
+#SLIRP_AUTORECONF = YES
 
-$(eval $(autotools-package))
+#batocera
+#$(eval $(autotools-package))
+$(eval $(meson-package))
