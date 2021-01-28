d3ad18f0ba1263c4b32c72ed99f730d3861ec9cd
Juliano Dorigão <jdorigao@gmail.com>
2021-01-28 09:11:39 -0400

    rpi-firmware bump to kernel 5.10.11

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 5bafb302877a08885a526fe081738548dd698ab8
+RPI_FIRMWARE_VERSION = 496e65477e06172ea20602e365d3790632c3cc06
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
