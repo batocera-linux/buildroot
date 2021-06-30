7650d516677ddb72567831504ac34f0f2184af30
liberodark <liberodark@gmail.com>
2021-06-30 11:18:43 +0200

    Bump intel driver

--- package/x11r7/xdriver_xf86-video-intel/xdriver_xf86-video-intel.mk
+++ package/x11r7/xdriver_xf86-video-intel/xdriver_xf86-video-intel.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 #batocera bump
-XDRIVER_XF86_VIDEO_INTEL_VERSION = a511f22cdec56504913b457a2e60dafee8e2b570
+XDRIVER_XF86_VIDEO_INTEL_VERSION = 31486f40f8e8f8923ca0799aea84b58799754564
 XDRIVER_XF86_VIDEO_INTEL_SITE = git://anongit.freedesktop.org/xorg/driver/xf86-video-intel
 XDRIVER_XF86_VIDEO_INTEL_LICENSE = MIT
 XDRIVER_XF86_VIDEO_INTEL_LICENSE_FILES = COPYING
