a93e3988926fcb17541374103eedc5cef033eac3
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-10-13 17:55:48 +0000

    fix sdl2 for ogoa
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/sdl2/sdl2.mk
+++ package/sdl2/sdl2.mk
@@ -164,7 +164,8 @@ SDL2_CONF_OPTS += --disable-alsa
 endif
 
 ifeq ($(BR2_PACKAGE_SDL2_KMSDRM),y)
-SDL2_DEPENDENCIES += libdrm mesa3d
+# batocera -mesa3d
+SDL2_DEPENDENCIES += libdrm
 SDL2_CONF_OPTS += --enable-video-kmsdrm
 else
 SDL2_CONF_OPTS += --disable-video-kmsdrm
