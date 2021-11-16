3fadc646d0e00e815297d2487acce3e2acad9779
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-11-16 17:59:56 +0000

    bump rtl8723bu
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/rtl8723bu/rtl8723bu.mk
+++ package/rtl8723bu/rtl8723bu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8723BU_VERSION = 9ce1c38439f4f574bb7adaf33949835c25536a28
+RTL8723BU_VERSION = d34603a39a7c925d810fe17c6e8dea5063d43a33
 RTL8723BU_SITE = $(call github,lwfinger,rtl8723bu,$(RTL8723BU_VERSION))
 RTL8723BU_LICENSE = GPL-2.0, proprietary (*.bin firmware blobs)
 
