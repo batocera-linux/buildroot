44c03f0871d7de9d428a283d01282a522a25bcfb
Juliano Dorigão <jdorigao@gmail.com>
2021-12-10 08:21:24 -0400

    bump: rpi-firmware to 5.10.83

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -3,8 +3,8 @@
 # rpi-userland
 #
 ################################################################################
-# Version.: Commits on Nov 23, 2021
-RPI_USERLAND_VERSION = f5404b2fcd5f36106db064a0bace585659219b09
+# batocera (update)
+RPI_USERLAND_VERSION = 14b90ff9d9f031391a299e6e006965d02bfd1bb1
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3-Clause
 RPI_USERLAND_LICENSE_FILES = LICENCE
