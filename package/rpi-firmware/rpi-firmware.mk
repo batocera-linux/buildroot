4db79b00375d12b3d2d5193102a2784179e69a12
Juliano Dorigão <jdorigao@gmail.com>
2022-01-11 09:17:29 -0400

    bump: rpi-firmware to 5.15.13

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # batocera (update)
-RPI_FIRMWARE_VERSION = fbbde90b7099af81d02a6c5ed034b987b716e7ca
+RPI_FIRMWARE_VERSION = 792913803fd6418cf0917d7c238038c62006e0e8
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
