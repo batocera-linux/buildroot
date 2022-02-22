56dcd680aa34ff88ef3e2dbe85aaba0ca0aa3548
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-02-22 20:22:02 +0000

    some br fixes
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # batocera (update)
-VULKAN_HEADERS_VERSION = v1.3.204
+VULKAN_HEADERS_VERSION = 1.3.204
 VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,v$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0
 VULKAN_HEADERS_LICENSE_FILES = LICENSE.txt
