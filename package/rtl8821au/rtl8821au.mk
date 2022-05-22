7cafeeb150460bc2fb7f1fb145470a3d9520ad39
Daniel Martin <dmanlfc@gmail.com>
2022-05-22 14:25:09 +1000

    update 8723bu & 8821 source for kernel 5.18

--- package/rtl8821au/rtl8821au.mk
+++ package/rtl8821au/rtl8821au.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 #batocera (change repo and hash)
-RTL8821AU_VERSION = e2df993bfd5b88e61770b461d94d0700cec2dbe5
+RTL8821AU_VERSION = 3647fb9eca41db330b01e59b4da2be9e38c9a77c
 RTL8821AU_SITE = $(call github,lwfinger,rtl8812au,$(RTL8821AU_VERSION))
 RTL8821AU_LICENSE = GPL-2.0
 RTL8821AU_LICENSE_FILES = LICENSE
