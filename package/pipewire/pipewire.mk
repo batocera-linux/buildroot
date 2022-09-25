2028f015ee43287a019a9665d1046c0cfdbbf8ef
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-09-25 21:08:57 +0000

    pipewire wireplumber
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/pipewire/pipewire.mk
+++ package/pipewire/pipewire.mk
@@ -29,10 +29,21 @@ PIPEWIRE_CONF_OPTS += \
 	-Dvideoconvert=enabled \
 	-Dvideotestsrc=enabled \
 	-Dvolume=enabled \
-	-Dsession-managers=media-session \
+	-Dsession-managers=wireplumber \
 	-Dlegacy-rtkit=false \
 	-Davb=disabled \
 	-Dlibcanberra=disabled
+
+# batocera
+# this is a not nice workaround
+# i don't know why meson uses bad ssl certificates and doesn't manage to download them
+define PIPEWIRE_DWD_DEPENDENCIES
+	mkdir -p $(@D)/subprojects/packagecache
+	$(HOST_DIR)/bin/curl -L https://www.lua.org/ftp/lua-5.4.4.tar.gz               -o $(@D)/subprojects/packagecache/lua-5.4.4.tar.gz
+	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/lua_5.4.4-1/get_patch -o $(@D)/subprojects/packagecache/lua_5.4.4-1_patch.zip
+endef
+PIPEWIRE_DEPENDENCIES += host-libcurl
+PIPEWIRE_PRE_CONFIGURE_HOOKS += PIPEWIRE_DWD_DEPENDENCIES
 
 # batocera
 PIPEWIRE_CONF_OPTS += --wrap-mode=default
