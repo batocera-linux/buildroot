c0ee8d144d32e876bcf748209e9152ebc90dda8a
dmanlfc <dmanlfc@gmail.com>
2022-10-25 22:23:50 +1000

    bump headers

--- package/sdl2/sdl2.mk
+++ package/sdl2/sdl2.mk
@@ -149,12 +149,13 @@ else
 SDL2_CONF_OPTS += --disable-video-x11-xcursor
 endif
 
-ifeq ($(BR2_PACKAGE_XLIB_LIBXINERAMA),y)
-SDL2_DEPENDENCIES += xlib_libXinerama
-SDL2_CONF_OPTS += --enable-video-x11-xinerama
-else
-SDL2_CONF_OPTS += --disable-video-x11-xinerama
-endif
+# batocera - needs removing, not a valid option
+#ifeq ($(BR2_PACKAGE_XLIB_LIBXINERAMA),y)
+#SDL2_DEPENDENCIES += xlib_libXinerama
+#SDL2_CONF_OPTS += --enable-video-x11-xinerama
+#else
+#SDL2_CONF_OPTS += --disable-video-x11-xinerama
+#endif
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBXI),y)
 SDL2_DEPENDENCIES += xlib_libXi
@@ -177,12 +178,13 @@ else
 SDL2_CONF_OPTS += --disable-video-x11-scrnsaver
 endif
 
-ifeq ($(BR2_PACKAGE_XLIB_LIBXXF86VM),y)
-SDL2_DEPENDENCIES += xlib_libXxf86vm
-SDL2_CONF_OPTS += --enable-video-x11-vm
-else
-SDL2_CONF_OPTS += --disable-video-x11-vm
-endif
+# batocera - needs removing, not a valid option
+#ifeq ($(BR2_PACKAGE_XLIB_LIBXXF86VM),y)
+#SDL2_DEPENDENCIES += xlib_libXxf86vm
+#SDL2_CONF_OPTS += --enable-video-x11-vm
+#else
+#SDL2_CONF_OPTS += --disable-video-x11-vm
+#endif
 
 else
 SDL2_CONF_OPTS += --disable-video-x11 --without-x
