95afde1c6913b7cf6758b26c26c5b5c3ca001c6f
Juliano Dorigão <jdorigao@gmail.com>
2023-02-24 10:32:04 -0400

    package/wlroots: add XWayland support
    The new option enables the XWayland support, so X11 application can be
    running on a wlroots based compositor. Also, do not enable the XWayland
    support if the X11 backend is active, as it is unrelated to it.
    
    Signed-off-by: Raphael Pavlidis <raphael.pavlidis@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    51ceb8

--- package/wlroots/wlroots.mk
+++ package/wlroots/wlroots.mk
@@ -34,11 +34,14 @@ ifeq ($(BR2_PACKAGE_HAS_LIBGBM),y)
 WLROOTS_CONF_OPTS += -Dallocators=gbm
 endif
 
-# batocera - add xwayland
 ifeq ($(BR2_PACKAGE_WLROOTS_X11),y)
 WLROOTS_BACKENDS += x11
+WLROOTS_DEPENDENCIES += libxcb xcb-util-wm xcb-util-renderutil xlib_libX11
+endif
+
+ifeq ($(BR2_PACKAGE_WLROOTS_XWAYLAND),y)
 WLROOTS_CONF_OPTS += -Dxwayland=enabled
-WLROOTS_DEPENDENCIES += libxcb xcb-util-wm xcb-util-renderutil xlib_libX11 xwayland
+WLROOTS_DEPENDENCIES += libxcb xcb-util-wm xwayland
 else
 WLROOTS_CONF_OPTS += -Dxwayland=disabled
 endif
