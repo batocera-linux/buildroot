7be051a8682dfaafb477f007475bfb20956e2d3b
Juliano Dorigão <jdorigao@gmail.com>
2023-02-23 21:18:50 -0400

    package/sway: bump to 1.8.1

--- package/sway/sway.mk
+++ package/sway/sway.mk
@@ -3,12 +3,12 @@
 # sway
 #
 ################################################################################
-
-SWAY_VERSION = 1.7
+# batocera (update)
+SWAY_VERSION = 1.8.1
 SWAY_SITE = $(call github,swaywm,sway,v$(SWAY_VERSION))
 SWAY_LICENSE = MIT
 SWAY_LICENSE_FILES = LICENSE
-SWAY_DEPENDENCIES = systemd host-pkgconf wlroots json-c pcre cairo pango
+SWAY_DEPENDENCIES = host-pkgconf wlroots json-c pcre2 cairo pango
 SWAY_CONF_OPTS = \
 	-Dwerror=false \
 	-Ddefault-wallpaper=false \
@@ -18,8 +18,9 @@ SWAY_CONF_OPTS = \
 	-Dswaybar=false \
 	-Dswaynag=false \
 	-Dtray=disabled \
-	-Dman-pages=disabled \
-	-Dsd-bus-provider=libsystemd
+	-Dman-pages=disabled
+# batocera - We don't want systemd
+#-Dsd-bus-provider=libsystemd
 
 ifeq ($(BR2_PACKAGE_WLROOTS_X11),y)
 SWAY_CONF_OPTS += -Dxwayland=enabled
@@ -33,5 +34,12 @@ SWAY_DEPENDENCIES += gdk-pixbuf
 else
 SWAY_CONF_OPTS += -Dgdk-pixbuf=disabled
 endif
+
+# batocera - Install only what is needed avoiding systemd files
+define SWAY_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D $(@D)/build/sway/sway         $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D $(@D)/build/swaymsg/swaymsg   $(TARGET_DIR)/usr/bin
+endef
 
 $(eval $(meson-package))
