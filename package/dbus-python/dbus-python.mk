978d023cc93542824d1f6eeff8d6ccc5f746b6f1
Christian <git-developer@users.noreply.github.com>
2023-02-26 05:51:00 +0100

    docs(dbus-python): mark as pip-compatible package

--- package/dbus-python/dbus-python.mk
+++ package/dbus-python/dbus-python.mk
@@ -24,7 +24,7 @@ HOST_DBUS_PYTHON_CONF_ENV = \
 	PYTHON_LIBS="`$(HOST_DIR)/bin/python3-config --ldflags`" \
 	PYTHON_EXTRA_LIBS="`$(HOST_DIR)/bin/python3-config --libs --embed`"
 
-# batocera ;
+# batocera ; #511
 define HOST_DBUS_PYTHON_MARK_AS_INSTALLED
 	touch $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/dbus_python-$(DBUS_PYTHON_VERSION).egg-info
 endef
