dd2e148f0290c5e065b01d8d45d95df05bd8ffd4
Juliano Dorigão <jdorigao@gmail.com>
2023-04-07 21:51:12 -0400

    package/{mesa3d, mesa3d-headers}: bump version to 23.0.2

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -3,9 +3,9 @@
 # mesa3d
 #
 ################################################################################
-
+# batocera (update)
 # When updating the version, please also update mesa3d-headers
-MESA3D_VERSION = 23.0.1
+MESA3D_VERSION = 23.0.2
 MESA3D_SOURCE = mesa-$(MESA3D_VERSION).tar.xz
 MESA3D_SITE = https://archive.mesa3d.org
 MESA3D_LICENSE = MIT, SGI, Khronos
