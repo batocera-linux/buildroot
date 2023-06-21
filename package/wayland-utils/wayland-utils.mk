4c556983486d9e0dd12d0934c9a3f264225a8fef
Juliano Dorigão <jdorigao@gmail.com>
2023-06-21 19:31:47 -0400

    package/wayland-utils: bump to 1.2.0

--- package/wayland-utils/wayland-utils.mk
+++ package/wayland-utils/wayland-utils.mk
@@ -3,8 +3,8 @@
 # wayland-utils
 #
 ################################################################################
-
-WAYLAND_UTILS_VERSION = 1.1.0
+# batocera (update)
+WAYLAND_UTILS_VERSION = 1.2.0
 WAYLAND_UTILS_SITE = https://gitlab.freedesktop.org/wayland/wayland-utils/-/releases/$(WAYLAND_UTILS_VERSION)/downloads
 WAYLAND_UTILS_SOURCE = wayland-utils-$(WAYLAND_UTILS_VERSION).tar.xz
 WAYLAND_UTILS_LICENSE = MIT
