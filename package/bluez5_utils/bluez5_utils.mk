2037da194f1c2226834aff8c52009d1a16b54e6b
Daniel Martin <dmanlfc@gmail.com>
2023-12-21 11:03:40 +1000

    update to 5.71 with the board patch

--- package/bluez5_utils/bluez5_utils.mk
+++ package/bluez5_utils/bluez5_utils.mk
@@ -6,7 +6,7 @@
 
 # Keep the version and patches in sync with bluez5_utils-headers
 # batocera - update
-BLUEZ5_UTILS_VERSION = 5.70
+BLUEZ5_UTILS_VERSION = 5.71
 BLUEZ5_UTILS_SOURCE = bluez-$(BLUEZ5_UTILS_VERSION).tar.xz
 BLUEZ5_UTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/bluetooth
 BLUEZ5_UTILS_INSTALL_STAGING = YES
