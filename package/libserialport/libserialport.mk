c29ad0d337a194a04bfc31300957d6aa7d348856
Jason Millard <jsm174@gmail.com>
2024-01-18 23:05:07 -0500

    libserialport: update to latest

--- package/libserialport/libserialport.mk
+++ package/libserialport/libserialport.mk
@@ -4,8 +4,10 @@
 #
 ################################################################################
 
-LIBSERIALPORT_VERSION = 0.1.1
-LIBSERIALPORT_SITE = http://sigrok.org/download/source/libserialport
+# batocera - update to support vpinball
+
+LIBSERIALPORT_VERSION = fd20b0fc5a34cd7f776e4af6c763f59041de223b
+LIBSERIALPORT_SITE = $(call github,sigrokproject,libserialport,$(LIBSERIALPORT_VERSION))
 LIBSERIALPORT_LICENSE = LGPL-3.0+
 LIBSERIALPORT_LICENSE_FILES = COPYING
 LIBSERIALPORT_INSTALL_STAGING = YES
