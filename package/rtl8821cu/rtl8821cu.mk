015d8cad3b8744848153fa92df333674af74a982
Daniel Martin <dmanlfc@gmail.com>
2024-03-02 13:15:48 +1000

    prepare for bump to kernel to 6.8

--- package/rtl8821cu/rtl8821cu.mk
+++ package/rtl8821cu/rtl8821cu.mk
@@ -3,8 +3,8 @@
 # rtl8821cu
 #
 ################################################################################
-# batocera - version bump for linux 6.7+
-RTL8821CU_VERSION = eb52ece92607394ecc22e96b41671ec3e5b1fa30
+# batocera - version bump for linux 6.8+
+RTL8821CU_VERSION = 5b39398e2de146edeb76716420f3288f508bea61
 RTL8821CU_SITE = $(call github,morrownr,8821cu-20210916,$(RTL8821CU_VERSION))
 RTL8821CU_LICENSE = GPL-2.0
 RTL8821CU_LICENSE_FILES = LICENSE
