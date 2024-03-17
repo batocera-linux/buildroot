9280260aa16c32aa97984d27c86098e4b7d81918
Juliano Dorigão <jdorigao@gmail.com>
2024-03-17 15:02:18 -0400

    package/pipewire: bump to 1.0.4

--- package/pipewire/pipewire.mk
+++ package/pipewire/pipewire.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # batocera - update
-PIPEWIRE_VERSION = 1.0.0
+PIPEWIRE_VERSION = 1.0.4
 PIPEWIRE_SOURCE = pipewire-$(PIPEWIRE_VERSION).tar.bz2
 PIPEWIRE_SITE = https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/$(PIPEWIRE_VERSION)
 PIPEWIRE_LICENSE = MIT, LGPL-2.1+ (libspa-alsa), GPL-2.0 (libjackserver)
