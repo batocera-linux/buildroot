35420f04634c810b84f93fbf2bb1b5aedad28a77
Daniel Martin <dmanlfc@gmail.com>
2024-05-27 23:40:58 +1000

    bump python future to fix a bug

--- package/python-future/python-future.mk
+++ package/python-future/python-future.mk
@@ -3,10 +3,10 @@
 # python-future
 #
 ################################################################################
-
-PYTHON_FUTURE_VERSION = 0.18.3
-PYTHON_FUTURE_SOURCE = future-$(PYTHON_FUTURE_VERSION).tar.gz
-PYTHON_FUTURE_SITE = https://files.pythonhosted.org/packages/8f/2e/cf6accf7415237d6faeeebdc7832023c90e0282aa16fd3263db0eb4715ec
+# batocera - bump
+PYTHON_FUTURE_VERSION = v1.0.0
+PYTHON_FUTURE_SOURCE = $(PYTHON_FUTURE_VERSION).tar.gz
+PYTHON_FUTURE_SITE = https://github.com/PythonCharmers/python-future/archive/refs/tags
 PYTHON_FUTURE_SETUP_TYPE = setuptools
 PYTHON_FUTURE_LICENSE = MIT
 PYTHON_FUTURE_LICENSE_FILES = LICENSE.txt
