69ccb2d1555e3cf64a03ff167511d055914e3a4f
Daniel Martin <dmanlfc@gmail.com>
2024-06-16 00:28:55 +1000

    wayland updates

--- package/wlroots/wlroots.mk
+++ package/wlroots/wlroots.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # batocera (update)
-WLROOTS_VERSION = 0.17.2
+WLROOTS_VERSION = 0.17.3
 WLROOTS_SITE = https://gitlab.freedesktop.org/wlroots/wlroots/-/releases/$(WLROOTS_VERSION)/downloads
 WLROOTS_LICENSE = MIT
 WLROOTS_LICENSE_FILES = LICENSE
