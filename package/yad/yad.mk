61dd959e153f94442f3f1d71ff00e43b6a3c6b29
Lawawa <122138258+RetroBoyCN@users.noreply.github.com>
2025-12-05 17:06:45 +0800

    Update the yad version
    Since the yad SourceForge repository is no longer maintained, we switched to the author’s GitHub repository. For now, we’re not updating to the latest version, in case some parameters are incompatible.

--- package/yad/yad.mk
+++ package/yad/yad.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-YAD_VERSION = 0.40.0
-YAD_SOURCE = yad-$(YAD_VERSION).tar.xz
-YAD_SITE = http://sourceforge.net/projects/yad-dialog/files
+YAD_VERSION = v0.42.0
+YAD_SITE = $(call github,v1cont,yad,$(YAD_VERSION))
 YAD_LICENSE = GPL-3.0
 YAD_LICENSE_FILES = COPYING
 YAD_DEPENDENCIES = host-intltool host-pkgconf $(TARGET_NLS_DEPENDENCIES)
 YAD_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
 YAD_CONF_OPTS = --enable-html=no
+
+# batocera add autoreconf & opts
+YAD_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_LIBGTK3_X11),y)
 YAD_DEPENDENCIES += libgtk3
