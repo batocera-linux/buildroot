120dc7cd64019f3073e63fd1087b682525cf4ab7
Daniel Martin <dmanlfc@gmail.com>
2026-07-18 15:16:42 +1000

    wlroots to 0.20.2

--- package/wlroots/wlroots.mk
+++ package/wlroots/wlroots.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WLROOTS_VERSION = 0.20.1
+WLROOTS_VERSION = 0.20.2
 WLROOTS_SITE = https://gitlab.freedesktop.org/wlroots/wlroots/-/releases/$(WLROOTS_VERSION)/downloads
 WLROOTS_LICENSE = MIT
 WLROOTS_LICENSE_FILES = LICENSE
