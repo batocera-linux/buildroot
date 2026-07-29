c28c74055b612e9d278016927cb28251049cac76
Daniel Martin <dmanlfc@gmail.com>
2026-07-29 17:05:10 +1000

    rpi-fw to support 6.18.39

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -3,8 +3,8 @@
 # rpi-firmware
 #
 ################################################################################
-# batocera (update) - Aligns to kernel version: 6.18.37
-RPI_FIRMWARE_VERSION = 958bfb0a9d14a4e5c29ed72124c0797788503c5a
+# batocera (update) - Aligns to kernel version: 6.18.39
+RPI_FIRMWARE_VERSION = 8402891f3ffba8a2f1fb8b975bccb9a2e5c01335
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
